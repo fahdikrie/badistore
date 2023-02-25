19dcd05102c6c05ef6b3cfbe0866ec53979a12a0
Fahdii Ajmalal Fikrie <fahdii.ajmalal91@ui.ac.id>
2023-02-26 01:43:30 +0700

    chore: add prettier, husky, & lint-staged

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  // default prettier config
+  trailingComma: 'es5',
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  // @trivago/prettier-plugin-sort-imports config
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  importOrder: ['^@/(.*)$', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
